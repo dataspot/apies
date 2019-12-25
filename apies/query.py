21857181d22d4d58781dca66ab24095917b32353
Adam Kariv <adam.kariv@gmail.com>
2019-12-25 09:19:56 +0200

    v1.1.0 Separate queries for separate doc types

--- apies/query.py
+++ apies/query.py
@@ -9,137 +9,142 @@ from .logger import logger
 class Query():
 
     def __init__(self, search_indexes):
-        self.q = {}
         self.types = list(search_indexes.keys())
         self.indexes = list(search_indexes.values())
+        self.q = dict((t, {}) for t in self.types)
 
     def __str__(self):
         return demjson.encode(self.q)
 
     def run(self, es_client: Elasticsearch, debug):
         if debug:
-            logger.debug('QUERY:\n%s', json.dumps(self.q, indent=2, ensure_ascii=False))
-        body = json.dumps(self.q)
+            logger.debug('QUERY (for %s):\n%s', self.types[0],
+                         json.dumps(self.q[self.types[0]], indent=2, ensure_ascii=False))
         body = ''.join(
             '{}\n{}\n'.format(
                 json.dumps(dict(index=index)),
-                body
+                json.dumps(self.q[t])
             )
-            for index in self.indexes
+            for t, index in zip(self.types, self.indexes)
         )
         return es_client.msearch(body)
 
-    def must(self):
-        return self.q.setdefault('query', {})\
-                     .setdefault('function_score', {})\
-                     .setdefault('query', {})\
-                     .setdefault('bool', {})\
-                     .setdefault('must', [])
+    def must(self, t):
+        return self.q[t].setdefault('query', {})\
+                        .setdefault('function_score', {})\
+                        .setdefault('query', {})\
+                        .setdefault('bool', {})\
+                        .setdefault('must', [])
 
-    def must_not(self):
-        return self.q.setdefault('query', {})\
-                     .setdefault('function_score', {})\
-                     .setdefault('query', {})\
-                     .setdefault('bool', {})\
-                     .setdefault('must_not', [])
+    def must_not(self, t):
+        return self.q[t].setdefault('query', {})\
+                        .setdefault('function_score', {})\
+                        .setdefault('query', {})\
+                        .setdefault('bool', {})\
+                        .setdefault('must_not', [])
 
     def apply_term(self, term, text_fields,
                    multi_match_type='most_fields', multi_match_operator='and'):
-        search_fields = [text_fields[type_name] for type_name in self.types]
-        search_fields = list(set().union(*search_fields))
-        search_fields = dict(
-            (k, [x[1] for x in search_fields if x[0] == k])
-            for k in ('exact', 'inexact', 'natural')
-        )
-        matchers = []
-
-        # Multimatch
-        matchers.append(dict(
-            multi_match=dict(
-                query=term,
-                fields=[f for f in search_fields['inexact']],
-                type=multi_match_type,
-                operator=multi_match_operator
+        for type_name in self.types:
+            search_fields = text_fields[type_name]
+            search_fields = dict(
+                (k, [x[1] for x in search_fields if x[0] == k])
+                for k in ('exact', 'inexact', 'natural')
             )
-        ))
+            matchers = []
 
-        # Common Terms
-        for field in search_fields['natural']:
-            if '^' in field:
-                name, boost = field.split('^')
-            else:
-                name, boost = field, 1.0
+            # Multimatch
             matchers.append(dict(
-                common={
-                    name: dict(
-                        query=term,
-                        boost=float(boost),
-                        cutoff_frequency=0.001,
-                    )
-                }
-            ))
-        # Tuples
-        parts = term.split()
-        parts = [term] + parts + [' '.join(z) for z in zip(parts[:-1], parts[1:])]
-        for field in search_fields['exact']:
-            matchers.append(dict(
-                terms={
-                    field: tuple(set(parts))
-                }
-            ))
-
-        # Apply boosters
-        if len(matchers) > 0:
-            self.must().append(dict(
-                bool=dict(
-                    should=matchers,
-                    minimum_should_match=1
+                multi_match=dict(
+                    query=term,
+                    fields=[f for f in search_fields['inexact']],
+                    type=multi_match_type,
+                    operator=multi_match_operator
                 )
             ))
+
+            # Common Terms
+            for field in search_fields['natural']:
+                if '^' in field:
+                    name, boost = field.split('^')
+                else:
+                    name, boost = field, 1.0
+                matchers.append(dict(
+                    common={
+                        name: dict(
+                            query=term,
+                            boost=float(boost),
+                            cutoff_frequency=0.001,
+                        )
+                    }
+                ))
+            # Tuples
+            parts = term.split()
+            parts = [term] + parts + [' '.join(z) for z in zip(parts[:-1], parts[1:])]
+            for field in search_fields['exact']:
+                matchers.append(dict(
+                    terms={
+                        field: tuple(set(parts))
+                    }
+                ))
+
+            # Apply boosters
+            if len(matchers) > 0:
+                self.must(type_name).append(dict(
+                    bool=dict(
+                        should=matchers,
+                        minimum_should_match=1
+                    )
+                ))
 
         return self
 
     def apply_scoring(self):
-        fs = self.q.setdefault('query', {}).setdefault('function_score', {})
-        fs.update(dict(
-            boost_mode='multiply',
-            field_value_factor=dict(
-                field='score',
-                modifier='sqrt',
-                missing=1
-            )
-        ))
+        for type_name in self.types:
+            fs = self.q[type_name].setdefault('query', {}).setdefault('function_score', {})
+            fs.update(dict(
+                boost_mode='multiply',
+                field_value_factor=dict(
+                    field='score',
+                    modifier='sqrt',
+                    missing=1
+                )
+            ))
         return self
 
     def apply_sorting(self, sort_fields, score_threshold):
         # Apply the scoring threshold - since we are no longer sorting by score, it is important to use a score
         # threshold so as not to get irrelevant results
-        self.q.setdefault('min_score', score_threshold)
-        if isinstance(sort_fields, str):
-            if sort_fields[0] == '-':
-                sort_fields = sort_fields[1:]
-                sort = {sort_fields: {'order': 'desc'}}
+        for type_name in self.types:
+            q = self.q[type_name]
+            q.setdefault('min_score', score_threshold)
+            if isinstance(sort_fields, str):
+                if sort_fields[0] == '-':
+                    sort_fields = sort_fields[1:]
+                    sort = {sort_fields: {'order': 'desc'}}
+                else:
+                    sort = {sort_fields: {'order': 'asc'}}
             else:
-                sort = {sort_fields: {'order': 'asc'}}
-        else:
-            sort = sort_fields
+                sort = sort_fields
 
-        # Then sort by the sort fields, for example - {'__last_modified_at': {'order': 'desc'}}
-        self.q.setdefault('sort', sort)
+            # Then sort by the sort fields, for example - {'__last_modified_at': {'order': 'desc'}}
+            q.setdefault('sort', sort)
 
         return self
 
     def apply_pagination(self, page_size, offset):
-        self.q.update({
-            'size': int(page_size),
-            'from': int(offset)
-        })
+        for type_name in self.types:
+            self.q[type_name].update({
+                'size': int(page_size),
+                'from': int(offset)
+            })
         return self
 
     def apply_highlighting(self):
-        self.q['highlight'] = dict(
-            fields={'*': {}}
-        )
+        for type_name in self.types:
+            self.q[type_name]['highlight'] = dict(
+                fields={'*': {}}
+            )
         return self
 
     def parse_filter_op(self, k, v):
@@ -199,48 +204,61 @@ class Query():
         if isinstance(filters, dict):
             filters = [filters]
 
-        should_clauses = []
+        should_clauses = dict()
         if isinstance(filters, list):
             for i in filters:
                 bool_clause = {}
+                type_name = 'all'
                 for k, v in i.items():
-                    clause, positive = self.parse_filter_op(k, v)
-                    if positive:
-                        bool_clause.setdefault('must', []).append(clause)
+                    if k == '_type':
+                        type_name = v
                     else:
-                        bool_clause.setdefault('must_not', []).append(clause)
-                should_clauses.append(dict(bool=bool_clause))
-        self.must().append(dict(bool=dict(should=should_clauses)))
+                        clause, positive = self.parse_filter_op(k, v)
+                        if positive:
+                            bool_clause.setdefault('must', []).append(clause)
+                        else:
+                            bool_clause.setdefault('must_not', []).append(clause)
+                should_clauses.setdefault(type_name, []).append(dict(bool=bool_clause))
+        for type_name in self.types:
+            self.must(type_name).append(
+                dict(
+                    bool=dict(
+                        should=should_clauses.get(type_name, []) + should_clauses.get('all', [])
+                    )
+                )
+            )
         return self
 
     def apply_time_range(self, from_date, to_date):
         if None not in (from_date, to_date):
-            self.must().extend([
-                dict(
-                    range=dict(
-                        __date_range_from=dict(
-                            lte=to_date
+            for type_name in self.types:
+                self.must(type_name).extend([
+                    dict(
+                        range=dict(
+                            __date_range_from=dict(
+                                lte=to_date
+                            )
                         )
-                    )
-                ),
-                dict(
-                    range=dict(
-                        __date_range_to=dict(
-                            gte=from_date
+                    ),
+                    dict(
+                        range=dict(
+                            __date_range_to=dict(
+                                gte=from_date
+                            )
                         )
-                    )
-                ),
-            ])
+                    ),
+                ])
         return self
 
     def apply_month_aggregates(self):
-        self.q.setdefault('aggs', {}).update(dict(
-            timeline=dict(
-                terms=dict(
-                    field='__date_range_months',
-                    size=2500,
-                    order=dict(_term='asc')
+        for type_name in self.types:
+            self.q[type_name].setdefault('aggs', {}).update(dict(
+                timeline=dict(
+                    terms=dict(
+                        field='__date_range_months',
+                        size=2500,
+                        order=dict(_term='asc')
+                    )
                 )
-            )
-        ))
+            ))
         return self
